261dd12b296864f1f31e1ffa5c38dbd499ec0526
Shianwei <pshianwei@gmail.com>
2022-04-05 23:56:45 +0800

    Added REST framework Request, Response and Status

--- drfproject/api/views.py
+++ drfproject/api/views.py
@@ -1,55 +1,62 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from rest_framework.parsers import JSONParser
 from .models import Article
 from .serializers import ArticleSerializer
-from django.views.decorators.csrf import csrf_exempt
+# from django.views.decorators.csrf import csrf_exempt
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
 
 # Create your views here.
-@csrf_exempt
+@api_view(['GET','POST'])
 def article_list(request):
     """
-    List all code snippets, or create a new snippet.
+    List all articles, or create a new article.
     """
     if request.method =="GET":
         articles = Article.objects.all()
         serializers = ArticleSerializer(articles, many=True)
-        return JsonResponse(serializers.data, safe=False)
+        return Response(serializers.data)
     elif request.method=="POST":
         # request is some sort like this <WSGIRequest: POST '/api/articles'>
         # the data is in the form of dictionary
-        data = JSONParser().parse(request)
-        serializers = ArticleSerializer(data=data)
+        # data = JSONParser().parse(request) << no need to parse data from the request object of REST framework
+        serializers = ArticleSerializer(data=request.data)
         if serializers.is_valid():
             serializers.save()
-            return JsonResponse(serializers.data, status=201)
-        return JsonResponse(serializers.errors, status=400)
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
+@api_view(["GET","PUT","DELETE"])
 def article_details(request, pk):
+    """
+    Retrieve, update or delete a article
+    """
     try:
         article = Article.objects.get(pk=pk)
     except Article.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method =="GET":
         serializers = ArticleSerializer(article)
-        return JsonResponse(serializers.data, safe=False)
+        return Response(serializers.data)
     elif request.method=="PUT":
-        data = JSONParser().parse(request)
+        # data = JSONParser().parse(request)
         # in order to perform and update you need to pass in the instance that you want to update into the constructor
         # by having partial=True, it allows partial update << Normally this is called PATCH
         # PUT is replaces all current representations of target resource
-        serializers = ArticleSerializer(article, data=data, partial=True)
+        serializers = ArticleSerializer(article, data=request.data, partial=True)
         if serializers.is_valid():
             serializers.save()
             # The HTTP 204 No Content success status response code indicates that a request has succeeded, 
             # but that the client doesn't need to navigate away from its current page. (save and continue editting)
-            return JsonResponse(serializers.data, status=204)
-        return JsonResponse(serializers.errors, status=400)
+            return Response(serializers.data, status=status.HTTP_204_NO_CONTENT)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method =="DELETE":
         article.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
         
 
